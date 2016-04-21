a474b47cc5b0315f174392bc5ad65598a2c8a5a9
Jared Smith <jared.smith88@me.com>
2016-04-20 18:38:21 -0700

    add handle disconnect logic

--- web/channels/video_channel.ex
+++ web/channels/video_channel.ex
@@ -2,12 +2,14 @@ defmodule Rumbl.VideoChannel do
   use Rumbl.Web, :channel
   alias Rumbl.AnnotationView
 
-  def join("videos:" <> video_id, _params, socket) do
+  def join("videos:" <> video_id, params, socket) do
+    last_seen_id = params["last_seen_id"] || 0 
     video_id = String.to_integer(video_id)
     video = Repo.get!(Rumbl.Video, video_id)
 
     annotations = Repo.all(
     from a in assoc(video, :annotations),
+    where: a.id > ^last_seen_id, 
     order_by: [asc: a.at, asc: a.id],
     limit: 200,
     preload: [:user]
@@ -17,7 +19,6 @@ defmodule Rumbl.VideoChannel do
      "annotation.json")}
    {:ok, resp, assign(socket, :video_id, video_id)}
   end
-
   def handle_in("new_annotation", params, socket) do
     user = Rumbl.Repo.get(Rumbl.User, socket.assigns.user_id)
 
