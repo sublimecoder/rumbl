2e1ad6849e34df0027f2c99d4448c5ea8f485a29
Jared Smith <jared.smith88@me.com>
2016-04-20 18:30:00 -0700

    add annotations

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -8,9 +8,17 @@ defmodule Rumbl.UserSocket do
   transport :websocket, Phoenix.Transports.WebSocket
   # transport :longpoll, Phoenix.Transports.LongPoll
 
-  def connect(_params, socket) do
-    {:ok, socket}
-  end
+  @max_age 2 * 7 * 24 * 60 * 60
 
-  def id(_socket), do: nil
+  def connect(%{"token" => token}, socket) do
+    case Phoenix.Token.verify(socket, "user socket", token, max_age: @max_age) do
+      {:ok, user_id} ->
+        {:ok, assign(socket, :user_id, user_id)}
+      {:error, _reason} ->
+        :error
+    end
+  end
+  def connect(_params, _socket), do: :error
+
+  def id(socket), do: "users_socket:#{socket.assigns.user_id}"
 end
--- web/static/js/socket.js
+++ web/static/js/socket.js
@@ -1,8 +1,8 @@
 import {Socket} from "phoenix"
 
 let socket = new Socket("/socket", {
-	params: {token: window.userToken},
-	logger: (kind, msg, data) => { console.log(`${kind}: ${msg}`, data) }
+  params: {token: window.userToken},
+  logger: (kind, msg, data) => { console.log(`${kind}: ${msg}`, data) }
 })
 
 export default socket
